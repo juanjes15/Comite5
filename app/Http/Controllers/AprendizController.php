3c821f6d2efc73a909ff1cb58db8ca9f44ebd8aa
Juan <juanjes15@gmail.com>
2023-08-27 22:12:23 -0500

    requests aprendiz

--- app/Http/Controllers/AprendizController.php
+++ app/Http/Controllers/AprendizController.php
@@ -5,62 +5,55 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreAprendizRequest;
 use App\Http\Requests\UpdateAprendizRequest;
 use App\Models\Aprendiz;
+use App\Models\Ficha;
 
 class AprendizController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    //Muestra un listado del recurso
     public function index()
     {
-        //
+        $aprendizs = Aprendiz::latest()->paginate(5);
+        return view('aprendizs.index', compact('aprendizs'))->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    //Muestra el formulario de creación de un nuevo recurso
     public function create()
     {
-        //
+        $fichas = Ficha::all();
+        return view('aprendizs.create', compact('fichas'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    //Almacena un recurso recién creado
     public function store(StoreAprendizRequest $request)
     {
-        //
+        Aprendiz::create($request->validated());
+        return redirect()->route('aprendizs.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
+    //Muestra el recurso especificado
     public function show(Aprendiz $aprendiz)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    //Muestra el formulario para editar el recurso especificado
     public function edit(Aprendiz $aprendiz)
     {
-        //
+        $fichas = Ficha::all();
+        return view('aprendizs.edit', compact('aprendiz', 'fichas'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    //Actualiza el recurso especificado en el almacenamiento
     public function update(UpdateAprendizRequest $request, Aprendiz $aprendiz)
     {
-        //
+        $aprendiz->update($request->validated());
+        return redirect()->route('aprendizs.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    //Elimina el recurso especificado del almacenamiento
     public function destroy(Aprendiz $aprendiz)
     {
-        //
+        $aprendiz->delete();
+        return redirect()->route('aprendizs.index');
     }
 }
--- app/Http/Requests/UpdateAprendizRequest.php
+++ app/Http/Requests/UpdateAprendizRequest.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Aprendiz;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateAprendizRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+    //Determinar si el usuario está autorizado a realizar esta solicitud
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
-     */
+    //Obtener las reglas de validación que se aplican a la solicitud
     public function rules(): array
     {
         return [
-            //
+            'apr_identificacion' => ['required', 'numeric', 'min:5', 'max:10', Rule::unique(Aprendiz::class, 'apr_identificacion')],
+            'apr_nombres' => ['required', 'string', 'max:255'],
+            'apr_apellidos' => ['required', 'string', 'max:255'],
+            'apr_email' => ['required', 'email', 'max:255', Rule::unique(Aprendiz::class, 'apr_email')],
+            'apr_telefono' => ['required', 'numeric', 'min:7', 'max:20'],
+            'apr_direccion' => ['required', 'string', 'max:255'],
+            'apr_fechaNacimiento' => ['required', 'date', 'before:today'],
+            'ficha_id' => ['required', 'exists:fichas,id']
         ];
     }
 }
