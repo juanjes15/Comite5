5be2d3eef3cfb0607d3567b6fb3403e44e83385d
JJ <juanjes15@gmail.com>
2023-10-02 11:41:04 -0500

    semillas

--- database/seeders/ArticuloSeeder.php
+++ database/seeders/ArticuloSeeder.php
@@ -59,5 +59,315 @@ class ArticuloSeeder extends Seeder
             'art_descripcion' => 'Ingreso',
             'capitulo_id' => 4,
         ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 11,
+            'art_descripcion' => 'Registro en el sistema de gestión académica',
+            'capitulo_id' => 4,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 12,
+            'art_descripcion' => 'Matrícula',
+            'capitulo_id' => 4,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 13,
+            'art_descripcion' => 'Trámite de las novedades académicas y administrativas',
+            'capitulo_id' => 4,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 14,
+            'art_descripcion' => 'Gestión de Novedades',
+            'capitulo_id' => 4,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 15,
+            'art_descripcion' => 'Certificación',
+            'capitulo_id' => 4,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 16,
+            'art_descripcion' => 'Expedición de documentos académicos',
+            'capitulo_id' => 4,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 17,
+            'art_descripcion' => 'Verificación de documentos académicos SENA para trámites de apostilla',
+            'capitulo_id' => 4,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 18,
+            'art_descripcion' => 'Solicitudes',
+            'capitulo_id' => 4,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 19,
+            'art_descripcion' => 'Participación y Cumplimiento',
+            'capitulo_id' => 5,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 20,
+            'art_descripcion' => 'Incumplimientos',
+            'capitulo_id' => 5,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 21,
+            'art_descripcion' => 'Incumplimiento justificado',
+            'capitulo_id' => 5,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 22,
+            'art_descripcion' => 'Incumplimiento injustificado',
+            'capitulo_id' => 5,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 23,
+            'art_descripcion' => 'Deserción',
+            'capitulo_id' => 5,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 24,
+            'art_descripcion' => 'Procedimiento en caso de deserción',
+            'capitulo_id' => 5,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 25,
+            'art_descripcion' => 'Faltas',
+            'capitulo_id' => 6,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 26,
+            'art_descripcion' => 'Faltas académicas',
+            'capitulo_id' => 6,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 27,
+            'art_descripcion' => 'Faltas disciplinarias',
+            'capitulo_id' => 6,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 28,
+            'art_descripcion' => 'Calificación de las faltas',
+            'capitulo_id' => 6,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 29,
+            'art_descripcion' => 'Criterios para calificar la falta',
+            'capitulo_id' => 6,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 30,
+            'art_descripcion' => 'Medidas formativas',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 31,
+            'art_descripcion' => 'Tipos de medidas formativas',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 32,
+            'art_descripcion' => 'Medidas formativas Académicas',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 33,
+            'art_descripcion' => 'Medidas formativas disciplinarias',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 34,
+            'art_descripcion' => 'Medidas Sancionatorias',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 35,
+            'art_descripcion' => 'Instancias de evaluación y aplicación de las medidas formativas y sanciones',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 36,
+            'art_descripcion' => 'Equipo ejecutor del programa de formación',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 37,
+            'art_descripcion' => 'Comité de evaluación y seguimiento del Centro de formación',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 38,
+            'art_descripcion' => 'Criterios para aplicación de sanciones',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 39,
+            'art_descripcion' => 'Procedimiento para la aplicación de sanciones',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 40,
+            'art_descripcion' => 'Citación al Aprendiz',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 41,
+            'art_descripcion' => 'Sesión del Comité de Evaluación y Seguimiento',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 42,
+            'art_descripcion' => 'Acto administrativo sancionatorio',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 43,
+            'art_descripcion' => 'Notificación al Aprendiz',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 44,
+            'art_descripcion' => 'Delegación',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 45,
+            'art_descripcion' => 'Recurso de reposición',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 46,
+            'art_descripcion' => 'Firmeza del acto administrativo',
+            'capitulo_id' => 7,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 47,
+            'art_descripcion' => 'Evaluación del Aprendizaje',
+            'capitulo_id' => 8,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 48,
+            'art_descripcion' => 'Las evidencias de aprendizaje',
+            'capitulo_id' => 8,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 49,
+            'art_descripcion' => 'Transparencia de la evaluación de aprendizaje',
+            'capitulo_id' => 8,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 50,
+            'art_descripcion' => 'Acompañamiento en el proceso evaluativo',
+            'capitulo_id' => 8,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 51,
+            'art_descripcion' => 'Juicios de la Evaluación',
+            'capitulo_id' => 8,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 52,
+            'art_descripcion' => 'Seguimiento de los resultados',
+            'capitulo_id' => 8,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 53,
+            'art_descripcion' => 'Inconformidad con la evaluación',
+            'capitulo_id' => 8,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 54,
+            'art_descripcion' => 'Representatividad de los aprendices',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 55,
+            'art_descripcion' => 'Representantes voceros de ficha',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 56,
+            'art_descripcion' => 'Requisitos y condiciones para ser Vocero de ficha',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 57,
+            'art_descripcion' => 'Responsabilidades del Vocero de ficha',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 58,
+            'art_descripcion' => 'Elección de los voceros de ficha',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 59,
+            'art_descripcion' => 'Revocatoria de la designación de los voceros',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 60,
+            'art_descripcion' => 'Suplencia de voceros',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 61,
+            'art_descripcion' => 'Representantes de Centro',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 62,
+            'art_descripcion' => 'Número de Representantes por Centro de Formación',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 63,
+            'art_descripcion' => 'Periodo de Representación',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 64,
+            'art_descripcion' => 'Requisitos y condiciones para postularse como Representante de los Aprendices del Centro',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 65,
+            'art_descripcion' => 'Verificación de requisitos y condiciones para postularse como Representante de los Aprendices del Centro',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 66,
+            'art_descripcion' => 'Responsabilidades del Representante de Aprendices en el Centro',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 67,
+            'art_descripcion' => 'Dependencia responsable del proceso de Elección de Representantes de aprendices',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 68,
+            'art_descripcion' => 'Procedimiento para elegir Representante de los Aprendices por Centro',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 69,
+            'art_descripcion' => 'Revocatoria de la designación del Representante de Aprendiz SENA',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 70,
+            'art_descripcion' => 'Suplencia de Representante de aprendices',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 71,
+            'art_descripcion' => 'La Dirección de Formación Profesional desarrollará un plan para divulgar el presente reglamento en la comunidad educativa',
+            'capitulo_id' => 9,
+        ]);
+        DB::table('articulos')->insert([
+            'art_numero' => 72,
+            'art_descripcion' => 'El presente Acuerdo rige a partir de la fecha de su publicación en el Diario Oficial y deroga el Acuerdo 0007 del 2012 y el acuerdo 002 de 2014',
+            'capitulo_id' => 9,
+        ]);
     }
 }
--- database/seeders/NumeralSeeder.php
+++ database/seeders/NumeralSeeder.php
@@ -10,6 +10,186 @@ class NumeralSeeder extends Seeder
     public function run(): void
     {
         DB::table('numerals')->insert([
+            'num_descripcion' => 'Suscribir al momento de asentar la matrícula el acta de compromiso como Aprendiz SENA.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Conocer y cumplir con el código de integralidad, el reglamento del aprendiz, los manuales, lineamientos, procedimientos y demás normas del SENA y sus dependencias.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Actuar siempre teniendo como base los principios, valores y procederes institucionales, interiorizando y expresando en sus actitudes y comportamientos los deberes del aprendiz SENA establecidos en este reglamento y asumidos en el momento de la matrícula; obrando con honestidad, respeto, responsabilidad, lealtad, justicia, compañerismo y solidaridad con la totalidad de los integrantes de la comunidad educativa y expresándose con respeto, cultura y educación, en forma directa, a través de medios, canales y/o espacios internos o externos que le facilita la entidad.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Registrar en el sistema de gestión académico y/o otros aplicativos de la entidad, los datos básicos y de contacto, garantizando la veracidad de la información registrada.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Actualizar de oficio o a solicitud de parte, la información reportada en los aplicativos del SENA en los que se adelanten trámites para aprendices, de acuerdo con la normatividad vigente.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Cumplir con todas las actividades formativas propias de su proceso de aprendizaje según modalidad y del plan de mejoramiento, definidas durante su etapa lectiva y productiva; presentando puntualmente según el cronograma del Programa, las actividades y evidencias de aprendizaje establecidas para la ruta de aprendizaje.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Acceder regularmente a los ambientes virtuales previstos para la formación y mantener una comunicación frecuente con el tutor.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Ser reconocido con una condición de discapacidad y que el Centro de formación realice los ajustes razonables para brindar apoyo en el proceso formativo',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Participar en las actividades complementarias o de profundización, relacionadas con el programa de formación, con el fin de gestionar su proceso de aprendizaje.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Dedicar al proceso de aprendizaje las horas semanales señaladas para cada programa, que permitan dar cumplimiento a las horas establecidas, teniendo en cuenta el cronograma y metodología establecidos por el Instructor-Tutor',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Justificar debidamente inasistencias y/o incumplimientos a las actividades de la formación, comunicando oportunamente al instructor o al ente responsable durante la etapa lectiva y productiva.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Participar activamente en procesos de retroalimentación de la calidad de la formación y presentar oportunamente ante la coordinación académica las dificultades que se prensenten en la ejecución de la formación, que no permitan la apropiación del conocimiento y un adecuado desempeño en cada competencia.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Cumplir con la reglamentación y directrices que se emitan para su participación en actividades programadas por el SENA, como salidas, pasantías técnicas, intercambios de aprendices a nivel nacional e internacional, así como en las demás de carácter lúdico-pedagógico.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Informar y hacer la solicitud, por escrito, al Coordinador Académico y registrar en el sistema de gestión de la formación oportunamente las solicitudes o novedades (Traslados, Aplazamiento, Retiro voluntario y Reingreso), que presente durante el proceso de aprendizaje, utilizando medios virtuales y/o físicos; para los programas de formación complementaria en modalidad virtual, realizar en los sistemas de información el retiro voluntario del programa o inscripción del mismo.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Hacer uso apropiado de los ambientes de formación (infraestructura, equipos, herramientas, recursos didácticos, técnicos, tecnológicos, bibliográficos), disponibles para su proceso de aprendizaje, asumiendo responsabilidad legal en situaciones de utilización inadecuada y uso indebido, que deterioran los ambientes de aprendizaje y generan detrimento patrimonial.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Participar en las reuniones que programe el Centro de Formación para seguimiento a las actividades desarrolladas durante el desarrollo de sus etapas formativas (lectiva y productiva). La no asistencia a estas reuniones debe justificarse mediante excusa comprobable.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Respetar los derechos de autor en los materiales, trabajos, proyectos y demás documentos entregados y/o generados en el proceso formativo.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Realizar personalmente las evaluaciones, investigaciones, actividades y prácticas de formación, haciendo uso de sus conocimientos, su esfuerzo personal, creatividad y autoría propia, absteniéndose de presentar como propios, escritos, documentos, ideas o resultados que no sean de su autoría.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Respetar los bienes y recursos culturales, naturales, físicos, digitales, de uso público y/o privado (en la entidad o fuera de ella) y velar por la conservación del ambiente sano, apoyando y/o colaborando en las acciones que adelante el Centro de Formación para su protección, conservación y buen uso.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Usar apropiadamente y promover el uso de los elementos de protección personal que correspondan a la ejecución de su formación, aplicando las buenas prácticas de seguridad y salud en el trabajo definidas por la entidad y por la normatividad para cuidar de su vida, salud e integridad, prevenir riesgos y promover ambientes seguros y saludables.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Cumplir con la reglamentación y directrices que se emitan por el SENA para la realización de la etapa productiva.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Portar permanentemente y en lugar visible el documento que lo identifica como Aprendiz SENA, durante su proceso de aprendizaje, renovarlo de acuerdo con las disposiciones vigentes y devolverlo al finalizar el programa o cuando se presenta retiro, aplazamiento o cancelación de la matrícula. En caso de pérdida de documento, el aprendiz debe formular la denuncia correspondiente, tramitar el duplicado y cancelar el valor respectivo con base en la normatividad dada por la Dirección General.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Portar el uniforme de manera decorosa dentro del Centro de Formación, en los ambientes donde se desarrollen actividades extracurriculares y entornos diferentes al académico. Así como en el desarrollo de la etapa productiva, cuando la empresa patrocinadora lo exija.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Ingresar a las plataformas virtuales institucionales, debidamente identificadas con el respectivo código de acceso, personal e instransferible. El usuario y la contraseña suministrado al aprendiz por la Entidad, para el acceso a las plataformas virtuales institucionales son de uso personal exclusivo, por lo tanto no debe transferirse a otras personas. El mal uso de esta información es de su competencia directa y asumirá por ello las responsabilidades correspondientes.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Hacer uso apropiado de los espacios y medios de comunicación institucionales y respetar a los integrantes de la comunidad educativa, siendo solidario, tolerante y veraz en la información que se publique en medios impresos o digitales; abstenerse de enviar material multimedia que contenga imágenes, videos, documentos o grabaciones que no sean objeto de las actividades de aprendizaje y cumplir a cabalidad la normatividad vigente del Estado colombiano en lo referente a delitos informáticos.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Participar en los foros de discusión y demás espacios de socialización de los ambientes virtuales institucionales en los que se desarrollan actividades formativas, siguiendo normas de comunicación digital, reconociendo, compartiendo y construyendo conocimiento con los integrantes de la comunidad educativa, contribuyendo con el aporte de conocimientos y destrezas en la solución y propuesta de los temas del programa.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Informar al instructor, coordinador, directivo o personal de apoyo, cualquier irregularidad que comprometa el buen nombre y normal marcha del Centro de Formación de la entidad, sus ambientes y medios de formación y de la comunidad educativa, o que considere sospechosa dentro de la Institución y en los ambientes de aprendizaje, permitiendo una actuación oportuna, preventiva o correctiva.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Obrar conforme al principio del respeto de los derechos de los demás evitando realizar y/o apoyar actos que limiten y/o afecten la dignidad, intimidad e integridad de los miembros de la comunidad educativa, como impedir el acceso a funcionarios y aprendices a los centros de formación y demás instalaciones del SENA.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Asumir con compromiso las responsabilidades recibidas y más aún si son de representación de su grupo, del centro de formación, de la regional, o de la misma institución frente a otras entidades.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
+            'num_descripcion' => 'Hacer entrega oportuna de toda la documentación requerida para poder ingresar, formarse y certificarse en el SENA según programa de formación.',
+            'num_categoria' => 'Disciplinaria',
+            'num_tipoFalta' => 'Grave',
+            'articulo_id' => 8,
+        ]);
+        DB::table('numerals')->insert([
             'num_descripcion' => 'Plagiar materiales, trabajos y demás documentos generados en los grupos de trabajo o producto del trabajo en equipo institucional, así como las fuentes bibliográficas consultadas en los diferentes soportes.',
             'num_categoria' => 'Academica',
             'num_tipoFalta' => 'Grave',
