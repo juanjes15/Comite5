3a01ccb1ba264bff8848c9f3697ec1d13806a8d3
JJ <juanjes15@gmail.com>
2023-11-21 21:07:15 -0500

    ordenv3

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,56 +4,6 @@
     <div class="h-full px-1 pb-4 overflow-y-auto bg-pattern">
         <ul class="space-y-1 text-base text-white font-bold">
             @if (auth()->user()->rol === 'Administrador')
-                <!-- Aprendiz -->
-                <li>
-                    <button type="button"
-                        class="flex items-center w-full p-3 transition duration-75 rounded-lg group bg-sena-700 hover:text-black hover:bg-sena-200"
-                        aria-controls="dropdown-aprendiz" data-collapse-toggle="dropdown-aprendiz">
-                        <svg class="flex-shrink-0 w-5 h-5 text-sena-400 transition duration-75 group-hover:text-black"
-                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                            viewBox="0 0 18 21">
-                            <path
-                                d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                        </svg>
-                        <span class="flex-1 ml-3 text-left whitespace-nowrap">Aprendiz</span>
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 1 4 4 4-4" />
-                        </svg>
-                    </button>
-                    <ul id="dropdown-aprendiz" class="hidden py-2 space-y-2">
-                        <li>
-                            <a class="flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group bg-sena-600 hover:text-black hover:bg-sena-200"
-                                href="{{ route('instructors.index') }}">Instructores</a>
-                        </li>
-                    </ul>
-                </li>
-                <!-- Instructor -->
-                <li>
-                    <button type="button"
-                        class="flex items-center w-full p-3 transition duration-75 rounded-lg group bg-sena-700 hover:text-black hover:bg-sena-200"
-                        aria-controls="dropdown-instructor" data-collapse-toggle="dropdown-instructor">
-                        <svg class="flex-shrink-0 w-5 h-5 text-sena-400 transition duration-75 group-hover:text-black"
-                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                            viewBox="0 0 18 21">
-                            <path
-                                d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                        </svg>
-                        <span class="flex-1 ml-3 text-left whitespace-nowrap">Instructor</span>
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 1 4 4 4-4" />
-                        </svg>
-                    </button>
-                    <ul id="dropdown-instructor" class="hidden py-2 space-y-2">
-                        <li>
-                            <a class="flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group bg-sena-600 hover:text-black hover:bg-sena-200"
-                                href="{{ route('insViews.sol1Ini') }}">Solicitar comité</a>
-                        </li>
-                    </ul>
-                </li>
                 <!-- Gestor Comité -->
                 <li>
                     <button type="button"
@@ -73,31 +23,6 @@
                         </svg>
                     </button>
                     <ul id="dropdown-gestor" class="hidden py-2 space-y-2">
-                        <li>
-                            <a class="flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group bg-sena-600 hover:text-black hover:bg-sena-200"
-                                href="{{ route('instructors.index') }}">Instructores</a>
-                        </li>
-                    </ul>
-                </li>
-                <!-- Subdirector -->
-                <li>
-                    <button type="button"
-                        class="flex items-center w-full p-3 transition duration-75 rounded-lg group bg-sena-700 hover:text-black hover:bg-sena-200"
-                        aria-controls="dropdown-subdirector" data-collapse-toggle="dropdown-subdirector">
-                        <svg class="flex-shrink-0 w-5 h-5 text-sena-400 transition duration-75 group-hover:text-black"
-                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                            viewBox="0 0 18 21">
-                            <path
-                                d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                        </svg>
-                        <span class="flex-1 ml-3 text-left whitespace-nowrap">Subdirector</span>
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 1 4 4 4-4" />
-                        </svg>
-                    </button>
-                    <ul id="dropdown-subdirector" class="hidden py-2 space-y-2">
                         <li>
                             <a class="flex items-center w-full p-2 transition duration-75 rounded-lg pl-11 group bg-sena-600 hover:text-black hover:bg-sena-200"
                                 href="{{ route('instructors.index') }}">Instructores</a>
